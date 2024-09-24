fc7db774708672f364d5257886d43447c09f4df3
Marina Riabogina <marina.ryabogina@yandex.ru>
2024-09-24 12:46:36 +0700

    внесены изменения в conftest.py переименован файл test_fixtures.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,14 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-from tests.test_fixtures import *
+pytest_plugins = "tests.fixtures"
 
 
 @pytest.fixture(scope="function")
 def browser():
-    print("\nstart browser for test..")
     options = Options()
     options.add_argument("--headless")
     browser = webdriver.Chrome(options=options)
     yield browser
-    print("\nquit browser..")
     browser.quit()
